0531fa7063d76f4d22359499d441c15d7c72c2fb
Student <lab-205@aptech-msg.edu>
2023-05-22 02:58:09 -0700

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:app/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Instagram Clone',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroudColor,
+      ),
+      home: Scaffold(body:  Text('Lets build Instagram Clone')),
+    );
+  }
+}
